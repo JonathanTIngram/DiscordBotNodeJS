d8d48036fe901b9b5d7a88130f76622341dc29f0
Jonathan Ingram <jingram0906@gmail.com>
2020-12-19 14:42:26 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ index.js
+
+ @author : Jonathan Ingram
+ Created : 12/19/2020
+ Project : Discord bot for nightshift server
+*/
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) =>{
+	if (msg.content === 'pog') {
+		msg.reply('poggeres');	
+	}
+});
+
+client.login('Nzg5OTMyMjc2NDc3MDY3Mjg0.X95Pwg.BlFZ4qmeDTPMEH-Rl5bV-XmsTUo');
